6d713359b922e220bb971a6130573e9e4d942faf
Nam Le <namkyodai@gmail.com>
2019-05-04 14:26:44 +0800

    change

--- birth_statistic.R
+++ birth_statistic.R
@@ -8,3 +8,7 @@ births.dow
 
 barchart(births.dow,ylab="Day of Week",col="black")
 
+#Mu???n xem li???t kê theo phuong pháp d???
+
+dob.dm.tbl=table(WK=births2006.smpl$DOB_WK,MM=births2006.smpl$DMETH_REC)
+dob.dm.tbl
